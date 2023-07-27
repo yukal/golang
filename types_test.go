a22731c7d8e5a161164e096936321d17bea99584
Alexander Yukal <yukal.alexander@gmail.com>
2023-07-27 18:50:18 +0300

    update tests

--- types_test.go
+++ types_test.go
@@ -7,6 +7,128 @@ import (
 )
 
 func TestTypes(t *testing.T) {
+	t.Run("Convert types within ranges", func(t *testing.T) {
+		t.Run("Uint8ToInt8Range", func(t *testing.T) {
+			t.Parallel()
+	
+			if res := Uint8ToInt8Range(MIN_UINT8); res != MIN_INT8 {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", MIN_INT8, res)
+			}
+	
+			if res := Uint8ToInt8Range(MAX_UINT8); res != MAX_INT8 {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", MAX_INT8, res)
+			}
+		})
+	
+		t.Run("Uint8ToInt16Range", func(t *testing.T) {
+			t.Parallel()
+	
+			if res := Uint8ToInt16Range(MIN_UINT8); res != int16(MIN_INT8) {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", int16(MIN_INT8), res)
+			}
+	
+			if res := Uint8ToInt16Range(MAX_UINT8); res != int16(MAX_INT8) {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", int16(MAX_INT8), res)
+			}
+		})
+	
+		t.Run("Uint8ToInt32Range", func(t *testing.T) {
+			t.Parallel()
+	
+			if res := Uint8ToInt32Range(MIN_UINT8); res != int32(MIN_INT8) {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", int32(MIN_INT8), res)
+			}
+	
+			if res := Uint8ToInt32Range(MAX_UINT8); res != int32(MAX_INT8) {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", int32(MAX_INT8), res)
+			}
+		})
+	
+		t.Run("Uint8ToInt64Range", func(t *testing.T) {
+			t.Parallel()
+	
+			if res := Uint8ToInt64Range(MIN_UINT8); res != int64(MIN_INT8) {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", int64(MIN_INT8), res)
+			}
+	
+			if res := Uint8ToInt64Range(MAX_UINT8); res != int64(MAX_INT8) {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", int64(MAX_INT8), res)
+			}
+		})
+	
+		t.Run("Uint8ToIntRange", func(t *testing.T) {
+			t.Parallel()
+	
+			if res := Uint8ToIntRange(MIN_UINT8); res != int(MIN_INT8) {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", int(MIN_INT8), res)
+			}
+	
+			if res := Uint8ToIntRange(MAX_UINT8); res != int(MAX_INT8) {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", int(MAX_INT8), res)
+			}
+		})
+	
+		t.Run("Int8ToUint8Range", func(t *testing.T) {
+			t.Parallel()
+	
+			if res := Int8ToUint8Range(MIN_INT8); res != MIN_UINT8 {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", MIN_UINT8, res)
+			}
+	
+			if res := Int8ToUint8Range(MAX_INT8); res != MAX_UINT8 {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", MAX_UINT8, res)
+			}
+		})
+	
+		t.Run("Int8ToUint16Range", func(t *testing.T) {
+			t.Parallel()
+	
+			if res := Int8ToUint16Range(MIN_INT8); res != uint16(MIN_UINT8) {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", uint16(MIN_UINT8), res)
+			}
+	
+			if res := Int8ToUint16Range(MAX_INT8); res != uint16(MAX_UINT8) {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", uint16(MAX_UINT8), res)
+			}
+		})
+	
+		t.Run("Int8ToUint32Range", func(t *testing.T) {
+			t.Parallel()
+	
+			if res := Int8ToUint32Range(MIN_INT8); res != MIN_UINT32 {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", MIN_UINT32, res)
+			}
+	
+			if res := Int8ToUint32Range(MAX_INT8); res != uint32(MAX_UINT8) {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", uint32(MAX_UINT8), res)
+			}
+		})
+	
+		t.Run("Int8ToUint64Range", func(t *testing.T) {
+			t.Parallel()
+	
+			if res := Int8ToUint64Range(MIN_INT8); res != MIN_UINT64 {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", MIN_UINT64, res)
+			}
+	
+			if res := Int8ToUint64Range(MAX_INT8); res != uint64(MAX_UINT8) {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", uint64(MAX_UINT8), res)
+			}
+		})
+	
+		t.Run("Int8ToUintRange", func(t *testing.T) {
+			t.Parallel()
+	
+			if res := Int8ToUintRange(MIN_INT8); res != MIN_UINT {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", MIN_UINT, res)
+			}
+	
+			if res := Int8ToUintRange(MAX_INT8); res != uint(MAX_UINT8) {
+				t.Errorf("Expect( %T(%[1]v) ) => Got( %T(%[2]v) )", uint(MAX_UINT8), res)
+			}
+		})
+	})
+
 	t.Run("Wrong Types", func(t *testing.T) {
 		t.Skip()
 
