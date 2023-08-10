535edc4d7f2a3ea3d34c3e8ace8f44b77df129c0
Alexander Yukal <yukal.alexander@gmail.com>
2023-08-10 15:56:03 +0300

    update demo: filter

--- demo_filter.go
+++ demo_filter.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"yu/golang/src"
 )
 
@@ -16,42 +17,42 @@ type Article struct {
 	Age      uint8
 	Height   uint16
 	Weight   uint16
-	Images   uint8
-	Phones   uint8
-	Date     uint16
+	Images   []string
+	Phones   []string
+	Date     time.Time
 }
 
 func DemoFilter() {
 	filter := &src.Filter{
 		Id:       map[string]uint64{"min": 1},
 		RegionId: map[string]uint8{"min": 1, "max": 25},
-		// Hash:     map[string]uint8{"minLen": 1},
-		// Link:     map[string]uint8{"minLen": 1},
-		// Title:    map[string]uint8{"minLen": 5},
-		// Message:  map[string]uint8{"minLen": 10},
-		// Sex:      map[string]uint8{"eq": 2},
+		Hash:     map[string]uint8{"minLen": 1},
+		Link:     map[string]uint8{"minLen": 1},
+		Title:    map[string]uint8{"minLen": 5},
+		Message:  map[string]uint8{"minLen": 10},
+		Sex:      map[string]uint8{"eq": 2},
 		Age:      map[string]uint8{"min": 18, "max": 45},
 		Height:   map[string]uint8{"min": 150, "max": 200},
 		Weight:   map[string]uint8{"min": 45, "max": 80},
-		// Images:   map[string]uint8{"minLen": 1},
-		// Phones:   map[string]uint8{"minLen": 1},
-		// Date:     map[string]uint16{"year": 2023},
+		Images:   map[string]uint8{"minLen": 1},
+		Phones:   map[string]uint8{"minLen": 1},
+		Date:     map[string]uint16{"year": 2023},
 	}
 
 	article := &Article{
-		Id:       0,
-		RegionId: 0,
-		Hash:     "",
-		Link:     "",
-		Title:    "",
-		Message:  "",
-		Sex:      0,
-		Age:      17,
-		Height:   145,
-		Weight:   35,
-		Images:   0,
-		Phones:   0,
-		Date:     0,
+		Id:       100,
+		RegionId: 10,
+		Hash:     "zJszhq8ck9qtZhh1IhyqCqrwxLUyjJsu",
+		Link:     "https://domain.com/data/",
+		Title:    "Title",
+		Message:  "Hello World!",
+		Sex:      2,
+		Age:      28,
+		Height:   165,
+		Weight:   60,
+		Images:   []string{"img1", "img2"},
+		Phones:   []string{"0001234567"},
+		Date:     time.Now(),
 	}
 
 	if !src.IsValid(filter, article) {
