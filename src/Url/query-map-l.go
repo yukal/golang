b8a17d8921c369d8720ee46b2400c442995b7c13
Alexander Yukal <yukal.alexander@gmail.com>
2023-09-23 14:46:18 +0300

    refactor(Url): code optimization

--- src/Url/query-map-l.go
+++ src/Url/query-map-l.go
@@ -8,15 +8,15 @@ import (
 
 func NewQueryMapL(urlQuery string) QueryMap {
 	params := strings.Split(urlQuery, "&")
+	paramsLen := len(params)
 
-	if len(params) < 1 {
+	if paramsLen < 1 {
 		return QueryMap{}
 	}
 
-	cache := make(QueryMap, len(params))
-	data := make(QueryMap, len(params))
+	cache := make(QueryMap, paramsLen)
+	data := make(QueryMap, paramsLen)
 
-	var depth uint
 	var steps uint
 	var chunks []string
 	var value any
@@ -24,58 +24,58 @@ func NewQueryMapL(urlQuery string) QueryMap {
 	qUnescaped, _ := url.QueryUnescape(params[0])
 	chunks, value = divideQueryParam(qUnescaped)
 	currKey := strings.Join(chunks, "/")
+	depth := len(chunks)
+	params = params[1:]
 
 	for {
 		steps++
 
-		if length := len(chunks); length > 1 {
-			nextKey := strings.Join(chunks[:length-1], "/")
+		if depth > 1 {
+			nextKey := strings.Join(chunks[:depth-1], "/")
 
 			// 1. The shortest way of recording parameters using an already built path that is in the cache
 
 			if reflect.ValueOf(cache[nextKey]).Kind() == reflect.Map {
-				depth += 2
 
-				currKey := chunks[length-1]
+				currKey := chunks[depth-1]
 				fullKey := nextKey + "/" + currKey
 
 				cache[fullKey] = value
 				cache[nextKey].(QueryMap)[currKey] = cache[fullKey]
 
-				chunks = chunks[length-1:]
+				depth = 1 // should assign the value at the next iteration
+				chunks = chunks[:depth:depth]
 
 			} else {
 
 				// 2. If the given path is not in the cache, then it would recursively build
 
-				depth++
-
 				cache[currKey] = value
+				value = QueryMap{chunks[depth-1]: value}
 
-				value = QueryMap{chunks[length-1]: value}
-				chunks = chunks[:length-1]
+				depth--
+				chunks = chunks[:depth:depth]
 
+				currKey = nextKey
 			}
-
-			currKey = nextKey
 
 			// 3. A tree branch has been created and will be stored in the root of the data object
 
 		} else {
-			depth++
 
 			if currKey == chunks[0] {
-				cache[currKey] = value
-				data[currKey] = value
-			} else {
 				cache[chunks[0]] = value
+				data[chunks[0]] = value
+			} else {
+				cache[currKey] = value
 			}
 
-			if params = params[1:]; len(params) > 0 {
+			if len(params) > 0 {
 				qUnescaped, _ := url.QueryUnescape(params[0])
 				chunks, value = divideQueryParam(qUnescaped)
 				currKey = strings.Join(chunks, "/")
-				depth = 0
+				depth = len(chunks)
+				params = params[1:]
 			} else {
 				return data
 			}
