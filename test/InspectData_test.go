df5876e5421d44e7a12487dda90383f01ec724c1
Alexander Yukal <yukal.alexander@gmail.com>
2023-09-24 22:05:47 +0300

    test: fix results storing

--- test/InspectData_test.go
+++ test/InspectData_test.go
@@ -2,7 +2,7 @@ package test
 
 import (
 	"net/url"
-	"strings"
+	"os"
 	"testing"
 	"yu/golang/src"
 	"yu/golang/src/Url"
@@ -12,36 +12,10 @@ import (
 // go test -bench=. -benchtime=1000x -benchmem
 
 func TestInspectData(t *testing.T) {
-	const expectInspectData = `
-Filter:
-  Data:
-    0:
-      RegionId: 1
-      Phone: "380001234567"
-      Age:
-        Min: 18
-        Max: 28
-      Nested:
-        Nested:
-          IsActual: true
-          IsUniq: false
-    1:
-      RegionId: 2
-      Phone: "380007654321"
-      Age:
-        Min: 21
-        Max: 41
-      Nested:
-        Nested:
-          IsActual: false
-          IsUniq: true
-  Text: "привіт!"
-  OrderBy:
-    0: "user_id DESC"
-    1: "created_at DESC"
-  Limit: 12
-  Offset: 6
-`
+	expectInspectData, err := os.ReadFile("InspectData_res.yml")
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	t.Parallel()
 
@@ -79,7 +53,7 @@ Filter:
 	}
 
 	var payload Payload
-	var expect = strings.TrimSpace(expectInspectData)
+	var expect = string(expectInspectData)
 
 	const query = `filter[data][0][regionId]=1&filter[data][0][phone]=380001234567&filter[data][0][age][min]=18&filter[data][0][age][max]=28&filter[data][0][nes][nes2][isActual]=true&filter[data][0][nes][nes2][isUniq]=0&filter[data][1][regionId]=2&filter[data][1][phone]=380007654321&filter[data][1][age][min]=21&filter[data][1][age][max]=41&filter[data][1][nes][nes2][isActual]=&filter[data][1][nes][nes2][isUniq]=true&filter[text]=%D0%BF%D1%80%D0%B8%D0%B2%D1%96%D1%82!&filter[orderBy][0]=user_id+DESC&filter[orderBy][1]=created_at+DESC&filter[limit]=12&filter[offset]=6`
 
