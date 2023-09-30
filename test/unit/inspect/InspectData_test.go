fd42408c67d2a82a27afa5e629ebe3a387926b5f
Alexander Yukal <yukal.alexander@gmail.com>
2023-09-30 13:44:45 +0300

    refactor(inspect): wrap as struct

--- test/unit/inspect/InspectData_test.go
+++ test/unit/inspect/InspectData_test.go
@@ -14,33 +14,35 @@ var result any
 // go test -run TestInspect ./test/unit/inspect
 
 func TestInspectData(t *testing.T) {
-	expect, err := test.ReadFile("./data/settings.yaml")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	wrongItems := 0
-
-	for n := 0; n < 8; n++ {
-		settings := app.MustLoadSettings("./data/settings.json")
-		yaml := app.InspectData(settings)
-
-		if yaml != expect {
-			wrongItems++
-			// t.Fatalf("Expect(\n%s)\n\nGot(\n%s)", expect, yaml)
-
-			// if err := os.WriteFile("./1-expect.yaml", []byte(expect), 0644); err != nil {
-			// 	t.Fatal(err)
-			// }
-
-			// if err := os.WriteFile("./2-got.yaml", []byte(yaml), 0644); err != nil {
-			// 	t.Fatal(err)
-			// }
+	t.Run("struct", func(t *testing.T) {
+		expect, err := test.ReadFile("./data/settings.yaml")
+		if err != nil {
+			t.Fatal(err)
 		}
-	}
 
-	if wrongItems > 0 {
-		msg := fmt.Sprintf("Generated settings are not match %d times", wrongItems)
-		t.Error(errors.New(msg))
-	}
+		wrongItems := 0
+
+		for n := 0; n < 8; n++ {
+			settings := app.MustLoadSettings("./data/settings.json")
+			yaml := app.InspectData(settings)
+
+			if yaml != expect {
+				wrongItems++
+				// t.Fatalf("Expect(\n%s)\n\nGot(\n%s)", expect, yaml)
+
+				// if err := os.WriteFile("./1-expect.yaml", []byte(expect), 0644); err != nil {
+				// 	t.Fatal(err)
+				// }
+
+				// if err := os.WriteFile("./2-got.yaml", []byte(yaml), 0644); err != nil {
+				// 	t.Fatal(err)
+				// }
+			}
+		}
+
+		if wrongItems > 0 {
+			msg := fmt.Sprintf("Generated settings are not match %d times", wrongItems)
+			t.Error(errors.New(msg))
+		}
+	})
 }
