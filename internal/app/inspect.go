ea56a903ae054b78dd8ebbdceb11a8d72fbe6247
Alexander Yukal <yukal.alexander@gmail.com>
2023-10-04 18:50:01 +0300

    refactor(inspect): code optimization - toText() merged with inspectRecursively() - simplified the output data by type - added MAX_DEPTH to limit deep tree parsing - added root key output - fixed processing of arrays - fixed parsing an empty nested objects - fixed space indentations

--- internal/app/inspect.go
+++ internal/app/inspect.go
@@ -8,35 +8,83 @@ import (
 	"strings"
 )
 
+const MAX_DEPTH = 16
+
 func InspectData(data any) string {
-	return strings.TrimSpace(
-		inspectRecursively(reflect.Indirect(reflect.ValueOf(data)), 0),
-	)
+	key := ""
+	value := reflect.Indirect(reflect.ValueOf(data))
+
+	if value.IsValid() {
+		key = value.Type().String()
+	} else {
+		key = value.Kind().String()
+	}
+
+	return inspectRecursively(key, value, 0)
 }
 
-func inspectRecursively(data reflect.Value, depth int) (text string) {
-	switch data.Kind() {
+func inspectRecursively(key string, value reflect.Value, depth int) (text string) {
+	separ := " "
+
+	if value.Kind() == reflect.Interface {
+		value = reflect.ValueOf(value.Interface())
+	}
+
+	switch value.Kind() {
+	// case reflect.Interface:
+	// 	text = fmt.Sprintf("%v\n", reflect.ValueOf(value.Interface()))
+	// 	text = inspectRecursively("", reflect.ValueOf(value.Interface()), cmap, depth)
+
+	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int,
+		reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint,
+		reflect.Float32, reflect.Float64,
+		reflect.Complex64, reflect.Complex128,
+		reflect.Bool,
+		reflect.Invalid:
+
+		text = fmt.Sprintf("%v\n", value)
+
+	case reflect.String, reflect.Chan, reflect.Func, reflect.UnsafePointer:
+		text = fmt.Sprintf("%#v\n", value)
+
+	case reflect.Uintptr:
+		text = fmt.Sprintf("uintptr(%#v)\n", value)
+
+	case reflect.Pointer:
+		// TODO: avoid infinite tree
+		text = inspectRecursively(key, value.Elem(), depth)
+
 	case reflect.Struct:
-		if data.Type().NumField() > 0 {
-			text = inspectStruct(data, 0, depth)
+		// TODO: avoid infinite tree
+
+		if value.Type().NumField() > 0 && depth < MAX_DEPTH {
+			text = inspectStruct(value, 0, depth+1)
+			separ = "\n"
+		} else {
+			text = value.Type().String() + "{}\n"
 		}
 
-	case reflect.Slice:
-		if data.Len() > 0 {
-			text = inspectSlice(data, 0, depth)
+	case reflect.Slice, reflect.Array:
+		if value.Len() > 0 && depth < MAX_DEPTH {
+			text = inspectSlice(value, 0, depth+1)
+			separ = "\n"
+		} else {
+			text = value.Type().String() + "{}\n"
 		}
 
 	case reflect.Map:
-		if keys := sortMapKeys(data.MapKeys()); len(keys) > 0 {
-			text = inspectMap(data, keys, depth)
+		// TODO: avoid infinite tree
+
+		if keys := value.MapKeys(); len(keys) > 0 && depth < MAX_DEPTH {
+			text = inspectMap(value, sortMapKeys(keys), depth+1)
+			separ = "\n"
+		} else {
+			text = value.Type().String() + "{}\n"
 		}
-
-	case reflect.Interface:
-		text = inspectRecursively(reflect.ValueOf(data.Interface()), depth)
-
-	default:
-		text = toText("", data, depth)
 	}
+
+	indent := strings.Repeat(" ", depth*2)
+	text = fmt.Sprintf("%s%s:%s%s", indent, key, separ, text)
 
 	return
 }
@@ -45,7 +93,7 @@ func inspectStruct(data reflect.Value, index, depth int) (text string) {
 	field := data.Type().Field(index)
 	value := reflect.Indirect(data).FieldByName(field.Name)
 
-	text = toText(field.Name, value, depth)
+	text = inspectRecursively(field.Name, value, depth)
 
 	if index < data.Type().NumField()-1 {
 		text += inspectStruct(data, index+1, depth)
@@ -55,7 +103,7 @@ func inspectStruct(data reflect.Value, index, depth int) (text string) {
 }
 
 func inspectSlice(data reflect.Value, index, depth int) (text string) {
-	text = toText(strconv.Itoa(index), data.Index(index), depth)
+	text = inspectRecursively(strconv.Itoa(index), data.Index(index), depth)
 
 	if index < data.Len()-1 {
 		text += inspectSlice(data, index+1, depth)
@@ -66,55 +114,10 @@ func inspectSlice(data reflect.Value, index, depth int) (text string) {
 
 func inspectMap(data reflect.Value, keys []string, depth int) (text string) {
 	value := data.MapIndex(reflect.ValueOf(keys[0]))
-	text = toText(keys[0], value, depth)
+	text = inspectRecursively(keys[0], value, depth)
 
 	if len(keys) > 1 {
 		text += inspectMap(data, keys[1:], depth)
-	}
-
-	return
-}
-
-func toText(key string, value reflect.Value, depth int) (text string) {
-	indent := strings.Repeat(" ", depth*2)
-
-	switch value.Kind() {
-	case reflect.Invalid:
-		text = fmt.Sprintf("%s%s: %#v\n", indent, key, value)
-
-	case reflect.Bool:
-		text = fmt.Sprintf("%s%s: %v\n", indent, key, value.Bool())
-
-	case reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Int:
-		text = fmt.Sprintf("%s%s: %v\n", indent, key, value.Int())
-
-	case reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uint:
-		text = fmt.Sprintf("%s%s: %v\n", indent, key, value.Uint())
-
-	case reflect.Float32, reflect.Float64:
-		text = fmt.Sprintf("%s%s: %v\n", indent, key, value.Float())
-
-	case reflect.Complex64, reflect.Complex128:
-		text = fmt.Sprintf("%s%s: %v\n", indent, key, value.Complex())
-
-	case reflect.String:
-		text = fmt.Sprintf("%s%s: %#v\n", indent, key, value.String())
-
-	case reflect.Chan:
-		text = fmt.Sprintf("%s%s: [chan]\n", indent, key)
-
-	case reflect.Func:
-		text = fmt.Sprintf("%s%s: [func]\n", indent, key)
-
-	case reflect.Uintptr, reflect.Pointer, reflect.UnsafePointer:
-		text = fmt.Sprintf("%s%s: %#v\n", indent, key, value)
-
-	case reflect.Interface:
-		// text = fmt.Sprintf("%s%s: %v\n", indent, key, reflect.ValueOf(value.Interface()))
-		text = toText(key, reflect.ValueOf(value.Interface()), depth)
-
-	case reflect.Struct, reflect.Slice, reflect.Array, reflect.Map:
-		text = fmt.Sprintf("%s%s:\n%s", indent, key, inspectRecursively(value, depth+1))
 	}
 
 	return
