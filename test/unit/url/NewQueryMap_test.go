d4cea850ee9c00541c4a1d9f5a47cae40bdde206
Alexander Yukal <yukal.alexander@gmail.com>
2023-12-23 21:07:55 +0200

    refactor: remove t.Parallel() #15

--- test/unit/url/NewQueryMap_test.go
+++ test/unit/url/NewQueryMap_test.go
@@ -13,7 +13,6 @@ import (
 func TestNewQueryMapR(t *testing.T) {
 
 	t.Run("query with empty value [1]", func(t *testing.T) {
-		t.Parallel()
 		const query = "data"
 		const expect = `{"data":""}`
 
@@ -25,7 +24,6 @@ func TestNewQueryMapR(t *testing.T) {
 	})
 
 	t.Run("query with empty value [2]", func(t *testing.T) {
-		t.Parallel()
 		const query = "data="
 		const expect = `{"data":""}`
 
@@ -37,7 +35,6 @@ func TestNewQueryMapR(t *testing.T) {
 	})
 
 	t.Run("rewrite existed value [1]", func(t *testing.T) {
-		t.Parallel()
 		const query = "data=1&data"
 		const expect = `{"data":""}`
 
@@ -49,7 +46,6 @@ func TestNewQueryMapR(t *testing.T) {
 	})
 
 	t.Run("rewrite existed value [2]", func(t *testing.T) {
-		t.Parallel()
 		const query = "data&data=1"
 		const expect = `{"data":"1"}`
 
@@ -61,7 +57,6 @@ func TestNewQueryMapR(t *testing.T) {
 	})
 
 	t.Run("parse object", func(t *testing.T) {
-		t.Parallel()
 		const query = "filter[fname]=Alex&filter[sname]=Yu&filter[bdate]=2020-01-01"
 		const expect = `{"filter":{"bdate":"2020-01-01","fname":"Alex","sname":"Yu"}}`
 
@@ -73,7 +68,6 @@ func TestNewQueryMapR(t *testing.T) {
 	})
 
 	t.Run("parse nested tree", func(t *testing.T) {
-		t.Parallel()
 		const query = "text=hello+world!&filter[slice][0][regionId]=2&filter[slice][0][phone]=380001234567&filter[slice][0][phone]=&filter[slice][0][data][min]=21&filter[slice][0][data][max]=41&filter[slice][0][data][conf][isActual]=true&filter[slice][0][data][conf][isUniq]=0&filter[slice][0][data][conf][min]=1&filter[slice][0][data][conf][max]=9&filter[slice][1][regionId]=4&filter[slice][1][phone]=380001234567&filter[slice][1][data][min]=4&filter[slice][1][data][max]=8&filter[slice][1][data][conf][isActual]=&filter[slice][1][data][conf][isUniq]=true&filter[text]=%D0%BF%D1%80%D0%B8%D0%B2%D1%96%D1%82!&filter[orderBy][0]=user_id+DESC&filter[orderBy][1]=created_at+DESC&filter[limit]=12&filter[offset]=6"
 		const expect = `{"filter":{"limit":"12","offset":"6","orderBy":{"0":"user_id DESC","1":"created_at DESC"},"slice":{"0":{"data":{"conf":{"isActual":"true","isUniq":"0","max":"9","min":"1"},"max":"41","min":"21"},"phone":"","regionId":"2"},"1":{"data":{"conf":{"isActual":"","isUniq":"true"},"max":"8","min":"4"},"phone":"380001234567","regionId":"4"}},"text":"привіт!"},"text":"hello world!"}`
 
@@ -88,7 +82,6 @@ func TestNewQueryMapR(t *testing.T) {
 func TestNewQueryMapL(t *testing.T) {
 
 	t.Run("query with empty value [1]", func(t *testing.T) {
-		t.Parallel()
 		const query = "data"
 		const expect = `{"data":""}`
 
@@ -100,7 +93,6 @@ func TestNewQueryMapL(t *testing.T) {
 	})
 
 	t.Run("query with empty value [2]", func(t *testing.T) {
-		t.Parallel()
 		const query = "data="
 		const expect = `{"data":""}`
 
@@ -112,7 +104,6 @@ func TestNewQueryMapL(t *testing.T) {
 	})
 
 	t.Run("rewrite existed value [1]", func(t *testing.T) {
-		t.Parallel()
 		const query = "data=1&data"
 		const expect = `{"data":""}`
 
@@ -124,7 +115,6 @@ func TestNewQueryMapL(t *testing.T) {
 	})
 
 	t.Run("rewrite existed value [2]", func(t *testing.T) {
-		t.Parallel()
 		const query = "data&data=1"
 		const expect = `{"data":"1"}`
 
@@ -136,7 +126,6 @@ func TestNewQueryMapL(t *testing.T) {
 	})
 
 	t.Run("parse object", func(t *testing.T) {
-		t.Parallel()
 		const query = "filter[fname]=Alex&filter[sname]=Yu&filter[bdate]=2020-01-01"
 		const expect = `{"filter":{"bdate":"2020-01-01","fname":"Alex","sname":"Yu"}}`
 
@@ -148,7 +137,6 @@ func TestNewQueryMapL(t *testing.T) {
 	})
 
 	t.Run("parse nested tree", func(t *testing.T) {
-		t.Parallel()
 		const query = "text=hello+world!&filter[slice][0][regionId]=2&filter[slice][0][phone]=380001234567&filter[slice][0][phone]=&filter[slice][0][data][min]=21&filter[slice][0][data][max]=41&filter[slice][0][data][conf][isActual]=true&filter[slice][0][data][conf][isUniq]=0&filter[slice][0][data][conf][min]=1&filter[slice][0][data][conf][max]=9&filter[slice][1][regionId]=4&filter[slice][1][phone]=380001234567&filter[slice][1][data][min]=4&filter[slice][1][data][max]=8&filter[slice][1][data][conf][isActual]=&filter[slice][1][data][conf][isUniq]=true&filter[text]=%D0%BF%D1%80%D0%B8%D0%B2%D1%96%D1%82!&filter[orderBy][0]=user_id+DESC&filter[orderBy][1]=created_at+DESC&filter[limit]=12&filter[offset]=6"
 		const expect = `{"filter":{"limit":"12","offset":"6","orderBy":{"0":"user_id DESC","1":"created_at DESC"},"slice":{"0":{"data":{"conf":{"isActual":"true","isUniq":"0","max":"9","min":"1"},"max":"41","min":"21"},"phone":"","regionId":"2"},"1":{"data":{"conf":{"isActual":"","isUniq":"true"},"max":"8","min":"4"},"phone":"380001234567","regionId":"4"}},"text":"привіт!"},"text":"hello world!"}`
 
--- test/unit/url/QueryUnmarshal_test.go
+++ test/unit/url/QueryUnmarshal_test.go
@@ -17,8 +17,6 @@ func TestQueryUnmarshal(t *testing.T) {
 		}
 
 		t.Run(`without sign "="`, func(t *testing.T) {
-			t.Parallel()
-
 			const query = "data"
 			const expect = `{"data":""}`
 
@@ -39,8 +37,6 @@ func TestQueryUnmarshal(t *testing.T) {
 		})
 
 		t.Run(`with sign "="`, func(t *testing.T) {
-			t.Parallel()
-
 			const query = "data="
 			const expect = `{"data":""}`
 
@@ -63,8 +59,6 @@ func TestQueryUnmarshal(t *testing.T) {
 	})
 
 	t.Run("parse nested tree", func(t *testing.T) {
-		t.Parallel()
-
 		const query = `filter[data][0][regionId]=1&filter[data][0][phone]=380001234567&filter[data][0][age][min]=18&filter[data][0][age][max]=28&filter[data][0][nes][nes2][isActual]=true&filter[data][0][nes][nes2][isUniq]=0&filter[data][1][regionId]=2&filter[data][1][phone]=380007654321&filter[data][1][age][min]=21&filter[data][1][age][max]=41&filter[data][1][nes][nes2][isActual]=&filter[data][1][nes][nes2][isUniq]=true&filter[text]=%D0%BF%D1%80%D0%B8%D0%B2%D1%96%D1%82!&filter[orderBy][0]=user_id+DESC&filter[orderBy][1]=created_at+DESC&filter[limit]=12&filter[offset]=6`
 		const expect = `{"filter":{"data":[{"regionId":1,"phone":"380001234567","age":{"min":18,"max":28},"nes":{"nes2":{"isActual":true,"isUniq":false}}},{"regionId":2,"phone":"380007654321","age":{"min":21,"max":41},"nes":{"nes2":{"isActual":false,"isUniq":true}}}],"text":"привіт!","orderBy":["user_id DESC","created_at DESC"],"limit":12,"offset":6}}`
 
